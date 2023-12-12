b79ea1a3d260e7d34d7c2fd48b6af7b893e0555d
Noboru Tokimi <92622004+tkmnbr@users.noreply.github.com>
2023-12-11 23:24:41 -0800

    Time: 7 ms (38.88%), Space: 10.2 MB (94.23%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
@@ -2,12 +2,26 @@ class Solution {
 public:
     int curr_max = 0;
     int maxProduct(vector<int>& nums) {
+        //Brute Force
+        // for(int i = 0; i < nums.size(); i++){
+        //     for(int j = 0; j < nums.size(); j++){
+        //         if(i != j && curr_max < (nums[i] - 1)*(nums[j] - 1))
+        //             curr_max = (nums[i] - 1)*(nums[j] - 1);
+        //     }
+        // }
+        // return curr_max;
+
+        //Find the two biggest numbers
+        int biggest = 0, second_biggest = 0;
         for(int i = 0; i < nums.size(); i++){
-            for(int j = 0; j < nums.size(); j++){
-                if(i != j && curr_max < (nums[i] - 1)*(nums[j] - 1))
-                    curr_max = (nums[i] - 1)*(nums[j] - 1);
+            if(biggest < nums[i]){
+                second_biggest = biggest;
+                biggest = nums[i];
+            }
+            else{
+                second_biggest = max(nums[i], second_biggest);
             }
         }
-        return curr_max;
+        return (biggest - 1)*(second_biggest - 1);
     }
 };
