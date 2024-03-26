7e9b29a388bcf740483485da6249b473c27046fb
Noboru Tokimi <92622004+tkmnbr@users.noreply.github.com>
2024-03-26 11:18:08 -0700

    Time: 24 ms (7.16%), Space: 27.7 MB (7.74%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,17 +1,8 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int count = 1;
         sort(nums.begin(), nums.end());
-        for(int i = 0; i < nums.size()-1; i++){
-            if(nums[i] == nums[i+1]){
-                count++;
-                if(count > nums.size()/2)
-                    return nums[i];
-            }
-            else
-                count = 1;
-        }
-        return 1;
+        int n = nums.size();
+        return nums[n / 2];
     }
 };
